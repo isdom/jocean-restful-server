77aad1db57d87eefc7150fb97039720dc2280532
ming.ma <isdom.maming@gmail.com>
2015-08-24 12:42:08 +0800

    移除废弃接口

--- src/main/java/org/jocean/restful/HeaderableOutputReactor.java
+++
@@ -1,8 +0,0 @@
-package org.jocean.restful;
-
-
-import java.util.Map;
-
-public interface HeaderableOutputReactor extends OutputReactor {
-    public void output(final Object representation, final Map<String, String> httpHeaders);
-}

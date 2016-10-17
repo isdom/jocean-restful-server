c154ba1c1aae01e970305e9822f18ab36301742b
ming.ma <isdom.maming@gmail.com>
2016-10-17 11:42:55 +0800

    HttpResponse 中支持 CookieParam 注解

--- src/main/java/org/jocean/restful/trade/ResponseProcessor.java
+++ src/main/java/org/jocean/restful/trade/ResponseProcessor.java
@@ -2,6 +2,7 @@ package org.jocean.restful.trade;
 
 import java.lang.reflect.Field;
 
+import javax.ws.rs.CookieParam;
 import javax.ws.rs.HeaderParam;
 
 import org.jocean.idiom.ExceptionUtils;
@@ -9,7 +10,9 @@ import org.jocean.idiom.ReflectUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import io.netty.handler.codec.http.HttpHeaders;
 import io.netty.handler.codec.http.HttpResponse;
+import io.netty.handler.codec.http.cookie.ServerCookieEncoder;
 import rx.functions.Action2;
 
 public class ResponseProcessor implements Action2<Object, HttpResponse> {
@@ -20,10 +23,38 @@ public class ResponseProcessor implements Action2<Object, HttpResponse> {
     public ResponseProcessor(final Class<?> clsResponse) {
         this._headerFields = 
             ReflectUtils.getAnnotationFieldsOf(clsResponse, HeaderParam.class);
+        this._cookieFields = 
+            ReflectUtils.getAnnotationFieldsOf(clsResponse, CookieParam.class);
     }
     
     @Override
     public void call(final Object respBean, final HttpResponse resp) {
+        fillHeaderParams(respBean, resp);
+        fillCookieParams(respBean, resp);
+    }
+
+    private void fillCookieParams(final Object respBean,
+            final HttpResponse resp) {
+        if (0 != this._cookieFields.length) {
+            for ( Field field : this._cookieFields ) {
+                try {
+                    final Object value = field.get(respBean);
+                    if ( null != value ) {
+                        final String cookiename = 
+                            field.getAnnotation(CookieParam.class).value();
+                        resp.headers().set(HttpHeaders.Names.SET_COOKIE, 
+                                ServerCookieEncoder.STRICT.encode(cookiename, value.toString()));
+                    }
+                } catch (Exception e) {
+                    LOG.warn("exception when get value from cookieparam field:[{}], detail:{}",
+                            field, ExceptionUtils.exception2detail(e));
+                }
+            }
+        }
+    }
+
+    private void fillHeaderParams(final Object respBean,
+            final HttpResponse resp) {
         if (0 != this._headerFields.length) {
             for ( Field field : this._headerFields ) {
                 try {
@@ -34,7 +65,7 @@ public class ResponseProcessor implements Action2<Object, HttpResponse> {
                         resp.headers().set(headername, value);
                     }
                 } catch (Exception e) {
-                    LOG.warn("exception when get value from field:[{}], detail:{}",
+                    LOG.warn("exception when get value from headerparam field:[{}], detail:{}",
                             field, ExceptionUtils.exception2detail(e));
                 }
             }
@@ -42,4 +73,5 @@ public class ResponseProcessor implements Action2<Object, HttpResponse> {
     }
     
     private final Field[] _headerFields;
+    private final Field[] _cookieFields;
 }
